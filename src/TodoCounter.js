d13834a2d215e768bdb48dc47f3d7bfe845f341a
Andres Araque <aaraqueamaya397@gmail.com>
2025-01-18 09:16:14 -0500

    [ADD] Estilos CSS en React

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,13 @@
+import './TodoCounter.css';
+
 function TodoCounter({ total, completed }) {
   return (
-    <h1>
-      Has completado {completed} de {total} TODOs
-    </h1>
+    <>
+      <h1>TODOs Andres</h1>
+      <h2>
+        Has completado {completed} de {total} TODOs
+      </h2>
+    </>
   );
 }
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,9 +1,11 @@
+import './TodoItem.css';
+
 function TodoItem(props) {
   return (
-    <li>
-      <span>V</span>
-      <p>{props.text}</p>
-      <span>X</span>
+    <li className="TodoItem">
+      <span className={`TodoItem-icon Icon-check ${props.completed && 'Icon-check--complete'}`}>V</span>
+      <p className={`${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
+      <span className="TodoItem-icon Icon-delete">X</span>
     </li>
   );
 }
