ae8f3849cb07e194bc0875f89f14e895b0c952c5
Andres Araque <aaraqueamaya397@gmail.com>
2025-01-21 13:19:29 -0500

    [ADD] Buscando TODOs

--- src/App.js
+++ src/App.js
@@ -16,11 +16,16 @@ const defaultTodos = [
 
 function App() {
   const [todos, setTodos] = React.useState(defaultTodos);
-
-  const [searchValue, setSearchValue] = React.useState('');
-
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length;
+
+  const [searchValue, setSearchValue] = React.useState('');
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+
+    return todoText.includes(searchText);
+  });
 
   return (
     <>
@@ -28,7 +33,7 @@ function App() {
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {defaultTodos.map((todo) => (
+        {searchedTodos.map((todo) => (
           <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
         ))}
       </TodoList>
