0e0c95cfa03fd1d12b2e167e2c97facb2520209d
Andres Araque <aaraqueamaya397@gmail.com>
2025-01-21 14:16:26 -0500

    [ADD] Completando y eliminando TODOs

--- src/App.js
+++ src/App.js
@@ -27,6 +27,22 @@ function App() {
     return todoText.includes(searchText);
   });
 
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text);
+
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text);
+
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <>
       <TodoCounter completed={completedTodos} total={totalTodos} />
@@ -34,7 +50,13 @@ function App() {
 
       <TodoList>
         {searchedTodos.map((todo) => (
-          <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
         ))}
       </TodoList>
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,9 +3,16 @@ import './TodoItem.css';
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <span className={`TodoItem-icon Icon-check ${props.completed && 'Icon-check--complete'}`}>V</span>
+      <span
+        className={`TodoItem-icon Icon-check ${props.completed && 'Icon-check--complete'}`}
+        onClick={props.onComplete}
+      >
+        V
+      </span>
       <p className={`${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
-      <span className="TodoItem-icon Icon-delete">X</span>
+      <span className="TodoItem-icon Icon-delete" onClick={props.onDelete}>
+        X
+      </span>
     </li>
   );
 }
