b0011c1591bdf2909b46091adc101e8d9c430e39
Andres Araque <aaraqueamaya397@gmail.com>
2025-01-25 13:52:13 -0500

    [ADD] Estados de carga y error

--- src/App/index.js
+++ src/App/index.js
@@ -15,7 +15,7 @@ import { useLocalStorage } from './useLocalStorage';
 //localStorage.removeItem('TODOS_V1');
 
 function App() {
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const { item: todos, saveItem: saveTodos, loading, error } = useLocalStorage('TODOS_V1', []);
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length;
 
@@ -45,6 +45,8 @@ function App() {
 
   return (
     <AppUI
+      loading={loading}
+      error={error}
       completedTodos={completedTodos}
       totalTodos={totalTodos}
       searchValue={searchValue}
