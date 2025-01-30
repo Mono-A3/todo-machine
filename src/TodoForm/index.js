6ce2d4392e635c39a63b5fab455ad5f3baf7aa56
Andres Araque <aaraqueamaya397@gmail.com>
2025-01-29 20:35:48 -0500

    [ADD] Crear TODOs: React Context dentro de React Portals

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,27 +1,41 @@
 import React from 'react';
 
+import { TodoContext } from '../TodoContext';
+
 import './TodoForm.css';
 
-function TodoForm({ setOpenModal }) {
+function TodoForm() {
+  const { addTodo, setOpenModal } = React.useContext(TodoContext);
+
+  const [newTodoValue, setNewTodoValue] = React.useState('');
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    addTodo(newTodoValue);
+    setOpenModal(false);
+  };
+
+  const onCancel = () => {
+    setOpenModal(false);
+  };
+
+  const onChange = (event) => {
+    setNewTodoValue(event.target.value);
+  };
+
   return (
-    <form
-      className="TodoForm"
-      onSubmit={(event) => {
-        event.preventDefault();
-      }}
-    >
+    <form className="TodoForm" onSubmit={onSubmit}>
       <label className="TodoForm-label">Escribe tu nuevo TODO</label>
 
-      <textarea className="TodoForm-textarea" placeholder="Cortar cebolla para el almuerzo" />
+      <textarea
+        value={newTodoValue}
+        onChange={onChange}
+        className="TodoForm-textarea"
+        placeholder="Cortar cebolla para el almuerzo"
+      />
 
       <div className="TodoForm-buttonContainer">
-        <button
-          type="button"
-          className="TodoForm-button TodoForm-button--cancel"
-          onClick={() => {
-            setOpenModal((state) => !state);
-          }}
-        >
+        <button type="button" className="TodoForm-button TodoForm-button--cancel" onClick={onCancel}>
           Cancelar
         </button>
 
