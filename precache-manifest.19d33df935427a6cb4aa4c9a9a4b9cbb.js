1cf8ca810763180e88bc655363c329db56913414
Andres Araque <aaraqueamaya397@gmail.com>
2025-01-29 21:02:29 -0500

    Updates

---
+++ precache-manifest.19d33df935427a6cb4aa4c9a9a4b9cbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4bbe7ba5b656b75f893ed6796b0273",
+    "url": "/curso-react-intro/index.html"
+  },
+  {
+    "revision": "2452f4a22a5ede5fb1a1",
+    "url": "/curso-react-intro/static/css/main.cc8b62a4.chunk.css"
+  },
+  {
+    "revision": "0b6ee0d6fbf548d38638",
+    "url": "/curso-react-intro/static/js/2.c0d933a0.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/curso-react-intro/static/js/2.c0d933a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2452f4a22a5ede5fb1a1",
+    "url": "/curso-react-intro/static/js/main.a1af7e3a.chunk.js"
+  },
+  {
+    "revision": "056824fdab9d7122fce9",
+    "url": "/curso-react-intro/static/js/runtime-main.132aa7c8.js"
+  }
+]);
