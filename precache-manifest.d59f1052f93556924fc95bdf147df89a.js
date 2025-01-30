97baa59ff1044010c1fa07c3afa98c210b9c3862
Andres Araque <aaraqueamaya397@gmail.com>
2025-01-30 09:06:02 -0500

    Updates

---
+++ precache-manifest.d59f1052f93556924fc95bdf147df89a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93159ed0c5c0be6f75dcec4aea7dc63",
+    "url": "/todo-machine/index.html"
+  },
+  {
+    "revision": "c8977f3b01fb58ca66e4",
+    "url": "/todo-machine/static/css/main.cc8b62a4.chunk.css"
+  },
+  {
+    "revision": "7615b0699efbfaa51594",
+    "url": "/todo-machine/static/js/2.78680727.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/todo-machine/static/js/2.78680727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8977f3b01fb58ca66e4",
+    "url": "/todo-machine/static/js/main.89ff3451.chunk.js"
+  },
+  {
+    "revision": "185e721547731bca435d",
+    "url": "/todo-machine/static/js/runtime-main.befd08ee.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/curso-react-intro/precache-manifest.19d33df935427a6cb4aa4c9a9a4b9cbb.js"
+  "/todo-machine/precache-manifest.d59f1052f93556924fc95bdf147df89a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/curso-react-intro/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-machine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
